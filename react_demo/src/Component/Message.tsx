280673910f56ff53abc13ccaabb5a81b11dfe482
Sain.Saji <Sain.Saji@trivium-esolutions.com>
2023-05-25 13:41:49 +0530

    Added Code formatting

--- react_demo/src/Component/Message.tsx
+++ react_demo/src/Component/Message.tsx
@@ -1,12 +1,11 @@
-
-function Message()
-{
-    const people="Nibbas and Nibbis"
-    return <div>
-        <h1 className="gradient-text">Yo Wassup!</h1>
-        <h4>Welcome {people}</h4>
-        </div>
-        
+function Message() {
+  const people = "Nibbas and Nibbis";
+  return (
+    <div>
+      <h1 className="gradient-text">Yo Wassup!</h1>
+      <h4>Welcome {people}</h4>
+    </div>
+  );
 }
 
-export default Message
+export default Message;
