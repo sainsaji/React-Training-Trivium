6129ba2075d2c951d1c65ac80c4b9fb7748f4124
Sain.Saji <Sain.Saji@trivium-esolutions.com>
2023-05-25 13:36:08 +0530

    Create Sample React App

---
+++ react_demo/src/App.tsx
@@ -0,0 +1,10 @@
+import Message from "./Component/Message"
+import './App.css'
+
+function App() {
+
+
+  return <Message></Message>
+}
+
+export default App
---
+++ react_demo/src/Component/Message.tsx
@@ -0,0 +1,12 @@
+
+function Message()
+{
+    const people="Nibbas and Nibbis"
+    return <div>
+        <h1 className="gradient-text">Yo Wassup!</h1>
+        <h4>Welcome {people}</h4>
+        </div>
+        
+}
+
+export default Message
